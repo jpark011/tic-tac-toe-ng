d3c634f07d3176274ef6a825a58deeadb8ee2a34
Jay Park <wngt3319@gmail.com>
2020-08-21 23:59:02 -0700

    Basic tic-tac-toe

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   templateUrl: './square.component.html',
   styleUrls: ['./square.component.scss']
 })
-export class SquareComponent implements OnInit {
+export class SquareComponent {
+  @Input() value: 'X' | 'O';
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() {
   }
 
 }
